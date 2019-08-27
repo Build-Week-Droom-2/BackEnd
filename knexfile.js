e98dede7049bcd6d3ebe242c02cf6a6f7ad1c2e9
Isaias Garcia-Ariza <isaiasgarciaariza@gmail.com>
2019-08-27 07:12:14 -0700

    postgress implementation

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,12 @@
+const localPg = {
+  host: 'localhost',
+  database: 'drooms',
+  user: 'recruiter',
+  password: 'rec78945'
+}
+
+const productionDbConnection = process.env.DATABASE_URL || localPg;
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -7,5 +16,21 @@ module.exports = {
       directory: './database/migrations',
     },
     seeds: { directory: './database/seeds' },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename:'./database/deoom.db3'
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: productionDbConnection,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
 };
